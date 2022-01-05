e26e72bb049d9107c9bac9303aa63af1beacaeb0
khinewaioo <khingwai10@gmail.com>
2022-01-05 23:08:59 +0900

    parent to child data communication

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-no="friend.phone"
+        :email-address="friend.email"
+        :is-fav="friend.isFav"
+        @toggle-favorite="toggleFavStatus"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "mnuel",
+          name: "Manuel Lorez",
+          phone: "1111",
+          email: "test@local",
+          isFav: true,
+        },
+        {
+          id: "julie",
+          name: "julie Lorez",
+          phone: "1111",
+          email: "julie@local",
+          isFav: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavStatus(friendId) {
+      const identifyFri = this.friends.find((friend) => friend.id === friendId);
+      identifyFri.isFav = !identifyFri.isFav;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFav ? "Fav": ""}}</h2>
+    <button @click="toggleFav"> Toggle Favorite</button>
+    <button @click="toggleDetail">
+      {{ detailVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailVisible">
+      <li><strong> Phone: </strong> {{ phoneNo }}</li>
+      <li><strong> Email: </strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {  // to access data from parent
+    id:{
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFav:
+    {
+      type: Boolean,
+      required: false
+    },
+  },
+  data() {
+    return {
+      detailVisible: false,
+      friend: {
+        id: "mnuel",
+        name: "Manuel Lorez",
+        phone: "1111",
+        email: "test@local",
+      },
+    };
+  },
+  methods: {
+    toggleDetail() {
+      this.detailVisible = !this.detailVisible;
+    },
+    toggleFav()
+    {
+      this.$emit('toggle-favorite',this.id); // to emit data to parent
+    }
+  },
+};
+</script>
